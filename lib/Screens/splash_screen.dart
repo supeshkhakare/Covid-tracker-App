4152a0d2300e2eedfd67051e06420ef430222c33
supesh khakare <yashkhakare05@gmail.com>
2025-07-16 19:24:26 +0530

    Initial commit

---
+++ lib/Screens/splash_screen.dart
@@ -0,0 +1,60 @@
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _animationcontroller = AnimationController(
+    duration: const Duration(seconds: 10),
+    vsync:
+        this, //The vsync parameter is used to improve performance by pausing animations when they're not visible.
+    //  It expects a class that implements TickerProvider, which SingleTickerProviderStateMixin provides.
+  )..repeat();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          AnimatedBuilder(
+              animation: _animationcontroller,
+              builder: (BuildContext context, Widget? child) {
+                return Transform.rotate(
+                    angle: _animationcontroller.value * 2.0 * math.pi,
+                    child: child);
+              },
+              child: Container(
+                height: 200,
+                width: 200,
+                child: Center(
+                    child: Image(image: AssetImage('Assets/Images/virus.png'))),
+              )),
+          SizedBox(
+            height: 100,
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Text(
+              "Covid\nTracker App",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blueGrey),
+            ),
+          )
+        ],
+      )),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:covid_tracker_app/Screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Covid Tracker App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
